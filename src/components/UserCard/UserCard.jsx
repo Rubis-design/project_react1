4f71ccfb352a35fcb47b2cb53bdf9a37ce4d4bf0
rubis-design <rubis.smm@gmail.com>
2024-09-25 21:01:27 +0300

    second commit

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,9 +1,12 @@
+import './UserCard.css';
 export function UserCard(props) {
   return (
-    <div className="card-wrapper">
-      <h1 className="card-title">User</h1>
-      <p>Username: {props.name}</p>
-      <p>Age: {props.age}</p>
+    <div className="card">
+      <div className="card-wrapper">
+        <h1 className="card-title">User</h1>
+        <p className="text">Username: {props.name}</p>
+        <p className="text">Age: {props.age}</p>
+      </div>
     </div>
   );
 }
